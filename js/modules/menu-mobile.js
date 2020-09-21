75e133e9a48af293ab426f1b6341bc84663f983b
Vitor Bragança <vitor_braganca@unigranrio.br>
2020-09-20 23:02:27 -0300

    Last update
    Corrigido o bug do menu mobile.
    Otimização para dispositivos móveis de diferentes tamanhos de tela.

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,21 +1,19 @@
 import outsideClick from './outsideClick.js';
 
 export default function initMenuMobile() {
-    const menuButton = document.querySelector('[data-menu="button"]');
-    const menuList = document.querySelector('[data-menu="list"]');
-    const eventos = ['click', 'touchstart'];
-
-    if(menuButton) {
-        function openMenu(event) {
-            console.log('Abriu')
-            menuList.classList.add('active');
-            menuButton.classList.add('active');
-            outsideClick(menuList, eventos, () => {
-                console.log('Fechou')
-                menuList.classList.remove('active');
-                menuButton.classList.remove('active');
-            })
-        }
-        eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
-    }
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart'];
+  
+  if(menuButton) {
+  function openMenu(event) {
+    menuList.classList.add('active');
+    menuButton.classList.add('active');
+    outsideClick(menuList, eventos, () => {
+      menuList.classList.remove('active');
+      menuButton.classList.remove('active');
+    })
+  }
+  eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Importando o modulo
 import initMenuMobile from './modules/menu-mobile.js';
 
+
 // Inicializando modulo
 initMenuMobile();
